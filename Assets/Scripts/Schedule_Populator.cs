4176c7c1b08a1cb6b436ebf67d64c2bb6c053166
Sove67 <45864422+Sove67@users.noreply.github.com>
2019-04-19 22:47:52 -0600

    Scripting Begins!
    Created the scripts for creating a schedule, checking which schedules occur today, and began the script for rendering the daily schedules.

---
+++ Assets/Scripts/Schedule_Populator.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Schedule_Populator : MonoBehaviour
+{
+    /*      
+     *      Master Populator Syntax
+     *  master_schedule.Add(new ItemCreate(name, start HHMM, end HHMM, next_appearance MMDD, interval DD, is_displayed_on_calendar, children));
+     *  
+     *      Unique List Index
+     *  List<Master_Schedule_Populator.ItemCreate> NAME
+     *  
+     */
+
+    public List<ItemCreate> master_schedule = new List<ItemCreate>();
+
+
+    // This class and its functon are used to create a schedule item.
+    public class ItemCreate
+    {
+        public string name { get; set; }
+        public string next_appearance { get; set; }
+        public string start { get; set; }
+        public string end { get; set; }
+        public string interval { get; set; }
+        public bool calendar { get; set; }
+        public List<string> children { get; set; }
+
+        public ItemCreate(string name, string start, string end, string next_appearance, string interval, bool calendar, List<string> children)
+        {
+            this.name = name;
+            this.start = start;
+            this.end = end;
+            this.next_appearance = next_appearance;
+            this.interval = interval;
+            this.calendar = calendar;
+            this.children = children;
+        }
+    }
+
+
+
+
+
+    public void Start()
+    {
+        PopulateDay();
+    }
+
+    public void PopulateDay()
+    {
+        var date = System.DateTime.Today.ToString("MMdd");
+        foreach (var item in master_schedule)
+        {
+            if (item.next_appearance == date)
+            {
+                // if this step is reached, the currently selected event is set to occur today
+                Debug.Log(item.name);
+            }
+        }
+    }
+
+
+
+
+    // Prints each part of the selected schedule to console
+    public void DebugScheduleItem(List<ItemCreate> schedule)
+    {
+        foreach (var component in schedule)
+        {
+            Debug.Log("Name: " + component.name);
+            Debug.Log("Start: " + component.start + " (24HR Format)");
+            Debug.Log("End: " + component.end + " (24HR Format)");
+            Debug.Log("Next Appearance: " + component.next_appearance + " (MMDD Format)");
+            Debug.Log("Interval: " + component.interval + " Days");
+            Debug.Log("Is Shown On Calendar: " + component.calendar);
+            if (component.children != null)
+            {
+                Debug.Log(component.name + " Child Tasks");
+                foreach (var children in component.children) { Debug.Log(children); }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Schedule_Renderer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Schedule_Renderer : MonoBehaviour
+{
+    public List<Schedule_Populator.ItemCreate> items;
+    public int scroll_input;
+
+    void Update()
+    {
+        GameObject container = GameObject.Find("Script Container");
+        Schedule_Populator schedule_populator = container.GetComponent<Schedule_Populator>();
+        items = schedule_populator.master_schedule;
+        RenderSchedule(items);
+
+        Scroll(scroll_input);
+    }
+
+    // Create each of the items as a combination of U.I. objects
+    private void RenderSchedule(List<Schedule_Populator.ItemCreate> items)
+    {
+        //Render the schedule in order of start time
+        foreach (var item in items)
+        {
+            var hour = int.Parse(item.start.Substring(0, 2));
+            var minute = int.Parse(item.start.Substring(2, 2));
+
+            Debug.Log("Name: " + item + "Hour: " + hour + "Minute: " + minute);
+        }
+        //Add "see tomorrow" button at bottom
+    }
+    
+    // Move the entire list using input in a distance calculation
+    private void Scroll(int input)
+    {
+
+    }
+}
