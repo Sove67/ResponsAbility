3651b692ed25736e8ea8361923c4324788cdde64
Kaleb Afseth <afseth598@gmail.com>
2019-06-17 16:05:24 -0600

    Text Sizer Updates & Pop Up Refining
    I updated Consistent Text script, some of the code was deprecated or something, causing it to no longer function. Its a bit janky atm, needs the rect masks to be disabled when starting the program. Also tweaked the Pop Up windows to look better.

--- Assets/Scripts/Consistent_Text.cs
+++ Assets/Scripts/Consistent_Text.cs
@@ -6,46 +6,63 @@ using UnityEngine.UI;
 public class Consistent_Text : MonoBehaviour
 {
 
-    // The following code is taken from Gwom's accepted answer at: https://answers.unity.com/questions/1301310/how-to-build-a-bestfit-font-size-for-a-group-of-ui.html
+    // The following code is a modified version of Gwom's accepted answer at: https://answers.unity.com/questions/1301310/how-to-build-a-bestfit-font-size-for-a-group-of-ui.html
+
+    // Sizing Variables
     public Text[] m_TextList;
-    private int m_Size = 1;
-    
+    public int m_Size = 1;
+
+    // Objects To Activate
+    public GameObject[] pop_ups;
+    public RectMask2D[] rect_masks;
+
     void Start()
     {
-        SetSizes();
-    }
+        // ApplySizes() doesnt function unless the text objects used are active and not masked. This allows the script to function properly
+        foreach (var obj in pop_ups)
+        { obj.SetActive(true); }
+        foreach (var obj in rect_masks)
+        { obj.enabled = false; }
 
-    public void SetSizes()
-    {
         foreach (var item in m_TextList)
-            item.resizeTextMaxSize = 1000;
+        { item.resizeTextMaxSize = 10000; }
 
         StartCoroutine(WaitFrame());
     }
 
+    // This Coroutine pauses the script for a frame, to allow the required data to be processed
     public IEnumerator WaitFrame()
     {
-        // returning 0 will make it wait 1 frame
-        // need to wait a frame in order for the layout to work out the size
         yield return 0;
         ApplySizes();
     }
 
     public void ApplySizes()
     {
+        Debug.Log("Group: " + name);
         // Get the first one
         if (m_TextList.Length > 0)
-        { m_Size = m_TextList[0].fontSize; }
+        { m_Size = m_TextList[0].cachedTextGenerator.fontSizeUsedForBestFit; }
 
         // See if any are smaller
         foreach (var item in m_TextList)
         {
-            if (item.fontSize < m_Size)
-            { m_Size = item.fontSize; }
+            Debug.Log("Item Text Size: " + item.cachedTextGenerator.fontSizeUsedForBestFit);
+
+            if (item.cachedTextGenerator.fontSizeUsedForBestFit < m_Size)
+            { m_Size = item.cachedTextGenerator.fontSizeUsedForBestFit; }
         }
+
+        Debug.Log("Final Size: " + m_Size);
 
         // Apply as max size to all of the images
         foreach (var item in m_TextList)
         { item.resizeTextMaxSize = m_Size; }
+
+        // ApplySizes() doesnt function unless the text objects used are active and not masked. This hides them once finished.
+        foreach (var window in pop_ups)
+        { window.SetActive(false); }
+        foreach (var obj in rect_masks)
+        { obj.enabled = true; }
     }
 }
