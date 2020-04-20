41493f825140737b8d2771bf48cdd9c74fc3a9e6
Kaleb Afseth <afseth598@gmail.com>
2020-04-20 14:42:50 -0600

    Fun Facts Revision
    Grammatical Fixes, and rewording to avoid framing the facts with a guaranteed impact

--- Assets/Scripts/Header.cs
+++ Assets/Scripts/Header.cs
@@ -26,18 +26,18 @@ public class Header : MonoBehaviour
         string[] funFacts = new string[]
         {
             "You can improve the quality of your study notes if you imagine you're making them for someone else.",
-            "20 minutes of exercise before an exam will improve your performance!",
+            "20 minutes of exercise before an exam can improve your performance.",
             "Chewing gum is actually a brain booster! One study found a 40 per cent increase in memory test scores.",
-            "Studying in a green surrounding or glancing at the colour green can make you more creative.",
-            "Drawing diagrams help you memorize them.",
+            "Studying in a green surrounding or glancing at the colour green can slightly improve your creativity.",
+            "Drawing a diagram can help you memorize it.",
             "You’re more likely to retain information from your class if you review what you’ve learned every day.",
             "Changing your environment helps you retain what you're studying.",
-            "Music helps improve language skills",
+            "Music helps improve language skills.",
             "Losing one nights sleep can impair reasoning and memory for up to four days.",
             "Foreign languages encourage growth in your brain.",
             "Studying the most challenging material right before sleeping makes it easier to remember the next morning.",
             "The harder something is to remember, the harder it is to forget.",
-            "To improve your memory of a subject, practice at spaced intervals over time"
+            "To improve your memory of a subject, you can practice at spaced intervals over time."
         };
 
         fact.text = "Did you know? " + funFacts[Random.Range(0, funFacts.Length - 1)];
